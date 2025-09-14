ed0a2327e8c010cd025ff93be55f87740d5fd1ac
prajapat04 <124580018+prajapat04@users.noreply.github.com>
2025-09-14 21:55:13 +0530

    Update authSeller.js

--- backend/middlewares/authSeller.js
+++ backend/middlewares/authSeller.js
@@ -2,17 +2,16 @@ import jwt from "jsonwebtoken";
 
 export const authSeller = (req, res, next) => {
   try {
-    const { sellerToken } = req.cookies;
-    if (!sellerToken) {
-      return res.status(401).json({ message: "Unauthorized", success: false });
+    const token = req.cookies?.sellerToken;
+    if (!token) {
+      return res.status(401).json({ message: "No seller token, Unauthorized", success: false });
     }
-    const decoded = jwt.verify(sellerToken, process.env.JWT_SECRET);
-    if(decoded.email === process.env.SELLER_EMAIL){ 
-      next();
-    }
-  
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.seller = decoded.id;
+    next();
   } catch (error) {
-    console.error("Authemtication error:", error);
-    return res.status(401).json({ message: "Unauthorized", success: false });
+    console.error("Seller Authentication error:", error.message);
+    return res.status(401).json({ message: "Seller Unauthorized", success: false });
   }
-}
+};
