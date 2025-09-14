da14a4738771badf059a3e918d037e21dbc33258
prajapat04 <124580018+prajapat04@users.noreply.github.com>
2025-09-14 12:50:22 +0530

    Update seller.routes.js

--- backend/routes/seller.routes.js
+++ backend/routes/seller.routes.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { isAuthSeller, sellerLogin, sellerLogout } from "../controllers/seller.controller.js";
-import { authSeller } from "../middlewares/authSeller.js";
-
+import { loginSeller, logoutSeller, isAuthSeller } from "../controllers/seller.controller.js";
+import { authSeller } from "../middleware/authSeller.js";
 
 const router = express.Router();
 
-router.post('/login', sellerLogin);
-router.get('/is-auth', authSeller, isAuthSeller);
-router.post('/logout', authSeller, sellerLogout);
+router.post("/login", loginSeller);
+router.post("/logout", logoutSeller);
+router.get("/is-auth", authSeller, isAuthSeller);
 
 export default router;
