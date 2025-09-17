e7caadb8ee60a37f91b4b25193fb596cc1a235f7
prajapat04 <124580018+prajapat04@users.noreply.github.com>
2025-09-17 09:39:43 +0530

    mUpdate seller.routes.js

--- backend/routes/seller.routes.js
+++ backend/routes/seller.routes.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { isAuthSeller, sellerLogin, sellerLogout } from "../controllers/seller.controller.js";
-
+import { authSeller } from "../middleware/authSeller.js"; // import middleware
 
 const router = express.Router();
 
-router.post('/login', sellerLogin);
-router.get('/is-auth', isAuthSeller, isAuthSeller);
-router.post('/logout', isAuthSeller, sellerLogout);
+// Public route
+router.post("/login", sellerLogin);
+
+// Protected routes
+router.get("/is-auth", authSeller, isAuthSeller);
+router.post("/logout", authSeller, sellerLogout);
 
 export default router;
