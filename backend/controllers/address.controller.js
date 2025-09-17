20f0444b4f53d782576fd8f60be428320918ede7
prajapat04 <124580018+prajapat04@users.noreply.github.com>
2025-09-17 21:29:31 +0530

    Update address.controller.js

--- backend/controllers/address.controller.js
+++ backend/controllers/address.controller.js
@@ -1,69 +1,28 @@
 import Address from "../models/address.model.js";
 
-// Add Address
 export const addAddress = async (req, res) => {
   try {
-    const userId = req.user; // set by authUser middleware
-    const {
-      firstName,
-      lastName,
-      email,
-      street,
-      city,
-      state,
-      zipCode,
-      country,
-      phone,
-    } = req.body;
-
-    // Validate required fields
-    if (
-      !firstName ||
-      !lastName ||
-      !email ||
-      !street ||
-      !city ||
-      !state ||
-      !zipCode ||
-      !country ||
-      !phone
-    ) {
-      return res.status(400).json({
-        success: false,
-        message: "All address fields are required",
-      });
-    }
-
-    const newAddress = await Address.create({
-      userId,
-      firstName,
-      lastName,
-      email,
-      street,
-      city,
-      state,
-      zipCode,
-      country,
-      phone,
+    const { address } = req.body;
+    const userId = req.user;
+     await Address.create({
+      ...address,
+      userId: userId,
     });
-
-    res.status(201).json({
-      success: true,
-      message: "Address added successfully",
-      address: newAddress,
-    });
+    res
+      .status(201)
+      .json({ success: true, message: "Address added successfully" });
   } catch (error) {
-    res.status(500).json({ success: false, message: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
-// Get Address
+//get address:// /api/address/get
 export const getAddress = async (req, res) => {
   try {
     const userId = req.user;
     const addresses = await Address.find({ userId });
     res.status(200).json({ success: true, addresses });
   } catch (error) {
-    res.status(500).json({ success: false, message: "Internal Server Error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
