078752ad69debac342a98ba64873417d8a46752b
prajapat04 <124580018+prajapat04@users.noreply.github.com>
2025-09-18 12:03:44 +0530

    Update authSeller.js

--- backend/middleware/authSeller.js
+++ backend/middleware/authSeller.js
@@ -3,13 +3,13 @@ import jwt from "jsonwebtoken";
 export const authSeller = (req, res, next) => {
   try {
      console.log("authSeller cookies:", req.cookies); 
-    const { token } = req.cookies; // read cookie
-    if (!token) {
+    const { sellerToken } = req.cookies; // read cookie
+    if (!sellerToken) {
       return res.status(401).json({ message: "Unauthorized", success: false });
     }
 
     // verify JWT
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(sellerToken, process.env.JWT_SECRET);
 
     // attach seller data to request
     req.seller = decoded;
@@ -19,5 +19,6 @@ export const authSeller = (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized", success: false });
   }
 };
+
 
 
