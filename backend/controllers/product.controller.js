2becd574ec2f648696ea768af86b2da66fbf59ca
prajapat04 <124580018+prajapat04@users.noreply.github.com>
2025-09-18 14:17:49 +0530

    Update product.controller.js

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -52,7 +52,7 @@ export const getProducts = async (req, res) => {
 
 export const getProductsById = async (req, res) => {
   try {
-    const { id } = req.body;
+    const { id } = req.params;
     const product = await Product.findById(id);
     if (!product) {
       return res
@@ -82,3 +82,4 @@ export const changeStock = async (req, res) => {
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
+
