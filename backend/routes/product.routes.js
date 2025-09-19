66df37f457dffc5beec0c2436a6cbb2e56d519cf
prajapat04 <124580018+prajapat04@users.noreply.github.com>
2025-09-19 13:28:23 +0530

    Update product.routes.js

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,19 +1,20 @@
 import express from "express";
 
 import { authSeller } from "../middleware/authSeller.js";
-import { addProduct, changeStock, getProducts, getProductsById } from "../controllers/product.controller.js";
+import { addProduct, changeStock, productList, productsById } from "../controllers/product.controller.js";
 import { upload } from "../config/multer.js";
 
 const router = express.Router();
 
 
-router.post("/add-product",authSeller, upload.array("image"), addProduct);
+router.post("/add", upload.array([image]), authSeller, addProduct);
 router.post("/stock", authSeller, changeStock);
-router.get("/list", getProducts);
-router.get("/:id", getProductsById);
+router.get("/list", productList);
+router.get("/id", productsById);
 
 
 
 export default router;
+
 
 
