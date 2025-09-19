84e000df63e1fd1c2ac1cfbd2252c3967d2b72f6
prajapat04 <124580018+prajapat04@users.noreply.github.com>
2025-09-19 23:15:32 +0530

    Update address.routes.js

--- backend/routes/address.routes.js
+++ backend/routes/address.routes.js
@@ -5,6 +5,7 @@ import { addAddress, getAddress } from "../controllers/address.controller.js";
 const router = express.Router();
 
 router.post("/add", authUser, addAddress);
-router.post("/get", authUser, getAddress);
+router.get("/get", authUser, getAddress);
 
 export default router;
+
