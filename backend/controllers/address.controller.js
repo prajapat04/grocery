92bf19c3d16d279f72f8958e62434cff122f5360
prajapat04 <124580018+prajapat04@users.noreply.github.com>
2025-09-19 09:09:00 +0530

    Update address.controller.js

--- backend/controllers/address.controller.js
+++ backend/controllers/address.controller.js
@@ -2,16 +2,16 @@ import Address from "../models/address.model.js";
 
 export const addAddress = async (req, res) => {
   try {
-    const { address } = req.body;
-    const userId = req.user;
+    const { address, userId } = req.body;
      await Address.create({
       ...address,
-      userId: userId,
+      userId
     });
     res
       .status(201)
       .json({ success: true, message: "Address added successfully" });
   } catch (error) {
+    console.log(error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
@@ -19,10 +19,11 @@ export const addAddress = async (req, res) => {
 //get address:// /api/address/get
 export const getAddress = async (req, res) => {
   try {
-    const userId = req.user;
+    const { userId } = req.body;
     const addresses = await Address.find({ userId });
     res.status(200).json({ success: true, addresses });
   } catch (error) {
+    console.log(error.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
